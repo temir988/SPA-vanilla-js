041fd28aa7b767639456c2fd8211d9945b8c5628
Temirlan <bamblebee988@gmail.com>
2019-01-07 19:11:31 +0600

    Step 4, add sort

--- pages/friends.js
+++ pages/friends.js
@@ -1,13 +1,35 @@
+import * as settings from '../settings.js';
+import * as utils from '../utils.js';
 import View from '../view.js';
 
 const resultsNode = document.querySelector('#results');
 let items = [];
+let sorting = settings.getFriendSort();
 
 export default {
     setData(newItems) {
         items = newItems;
     }, 
+
+    setSorting(newSorting) {
+        sorting = newSorting;
+        settings.setFriendSort(newSorting);
+    },
+
     render() {
+        if (sorting === 'name') {
+            utils.sortByName(items);
+        } else if (sorting === 'bday') {
+            utils.sortByBDate(items);
+        }
+
         resultsNode.innerHTML = View.render('friends', { list: items });
+
+        const sort = resultsNode.querySelector('[data-role=sort]');
+        sort.value = sorting;
+        sort.addEventListener('change', e => {
+            this.setSorting(e.target.value);
+            this.render();
+        });
     }
 }
